6cc11921821026b26877a43112f4976278e9a2fa
cosmos-explorer <lian.yang.work@gmail.com>
2023-10-07 07:48:12 +0200

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,99 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
+import Image from 'next/image';
+import slackLogo from './public/slack-new-logo.svg';
+import logoImg from '/public/logo.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  docsRepositoryBase: 'https://github.com/featbit/featbit-docs/tree/main',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – FeatBit'
+      }
+    }
+  },
+  logo: () => (
+    <>
+      <Image
+          priority
+          src={logoImg}
+          width={33}
+          alt="FeatBit"
+      />
+      <h1 style={{ marginLeft: '9px', fontWeight: 800, fontSize: '24px',  lineHeight: '33px'}}>
+        FeatBit Docs
+      </h1>
+    </>
+  ),
+  head: function Head() {
+    const { title } = useConfig()
+    const socialCard = 'https://dashboard.featbit.co/og.png'
+
+    return (
+        <>
+            <meta name="msapplication-TileColor" content="#fff" />
+            <meta name="theme-color" content="#fff" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta httpEquiv="Content-Language" content="en" />
+            <meta
+                name="description"
+                content="Open Source Feature Flags Management Service, Friendly for Self Hosted, Azure, AWS, GCP"
+            />
+            <meta
+                name="og:description"
+                content="Open Source Feature Flags Management Service, Friendly for Self Hosted, Azure, AWS, GCP"
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content={socialCard} />
+            <meta name="twitter:site:domain" content="featbit.co" />
+            <meta name="twitter:url" content="https://featbit.co" />
+            <meta
+                name="og:title"
+                content={title ? title + ' – FeatBit' : 'FeatBit'}
+            />
+            <meta name="og:image" content={socialCard} />
+            <meta name="apple-mobile-web-app-title" content="Nextra" />
+            <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+            <link rel="icon" href="/logo.png" type="image/png" />
+            <link
+                rel="icon"
+                href="/icon.svg"
+                type="image/svg+xml"
+                media="(prefers-color-scheme: dark)"
+            />
+            <link
+                rel="icon"
+                href="/local.png"
+                type="image/png"
+                media="(prefers-color-scheme: dark)"
+            />
+        </>
+    );
+  },
+  editLink: {
+    text: 'Edit this page on GitHub →'
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'feedback'
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/featbit/featbit-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    icon:  <Image priority src={slackLogo} width={24} alt="FeatBit Slack" height={24} />,
+    link: 'https://join.slack.com/t/featbit/shared_invite/zt-1ew5e2vbb-x6Apan1xZOaYMnFzqZkGNQ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: `© ${new Date().getFullYear()} FeatBit`,
+  },
+  darkMode: true,
+  nextThemes: {
+    defaultTheme: "dark",
   },
 }
 
