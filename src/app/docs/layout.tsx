94fb8bd57f1f247c06543d6638b807770e83cb6e
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2026-08-21 20:53:42 +0200

    采用上下结构

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,10 +1,12 @@
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
+  const { nav, ...options } = baseOptions();
+
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout tree={source.getPageTree()} {...options} nav={{ ...nav, mode: 'top' }}>
       {children}
     </DocsLayout>
   );
